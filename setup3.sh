b0188e65c2d04fcae00251c61b0bb53f01a9c20c
Seok-Hyeon Han <shhan0226@gmail.com>
2019-12-10 18:47:54 +0900

    Update chown -R stack /opt/stack/.cache [setup3.sh]

--- setup3.sh
+++ setup3.sh
@@ -2,6 +2,7 @@ echo ---------------------------------setup3------------------------------------
 cd /opt/stack
 echo [__download__]
 git clone https://git.openstack.org/openstack-dev/devstack
+chown -R stack /opt/stack/.cache
 cd devstack/
 cd samples
 cp local.conf ../.
