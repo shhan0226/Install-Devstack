4923cc9f69db1eb8581ceaef39a980e7d117e4b2
ubuntu <Seok-Hyeon>
2019-12-11 11:15:48 +0900

    Update pip install --upgrade pip [setup1.sh]

--- setup1.sh
+++ setup1.sh
@@ -7,6 +7,7 @@ sudo apt-get -y update
 echo [__pip__]
 pip install --upgrade pip
 sudo apt -y install python-pip
+sudo pip install --upgrade pip
 echo [__git__]
 sudo apt-get -y install git
 echo [__mysql__]
