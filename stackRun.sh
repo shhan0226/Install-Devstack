33ba1e379db0ea513db59e24701e4496fa0b991e
ubuntu <Seok-Hyeon>
2019-12-11 11:12:07 +0900

    Update chown -R stack:stack /opt/stack/.cache [stackRun.sh]

--- stackRun.sh
+++ stackRun.sh
@@ -2,5 +2,6 @@ echo --------------------------------stack_run----------------------------------
 chmod -R 777 /opt/stack/devstack
 chmod -R 777 /opt/stack
 chmod -R 777 /opt/
+chown -R stack:stack /opt/stack/.cache
 cd /opt/stack/devstack
 ./stack.sh
