72efb56921030033e7c87413afa650048f77ade0
Seok-Hyeon Han <shhan0226@gmail.com>
2019-12-17 03:06:07 +0900

    Update stacRun.sh

--- stackRun.sh
+++ stackRun.sh
@@ -2,7 +2,7 @@ echo --------------------------------stack_run----------------------------------
 sudo chmod -R 777 /opt/stack/devstack
 sudo chmod -R 777 /opt/stack
 sudo chmod -R 777 /opt/
-sudo apt-get install git python-pip
+sudo apt-get install git python-pip -y
 sudo pip install --upgrade pip
 echo chown -R stack:stack /opt/stack/.cache
 sudo chown -R stack:stack /opt/stack/.cache
@@ -10,4 +10,4 @@ cd /opt/stack/
 ls -al
 cd /opt/stack/devstack
 virtualenv /opt/stack/requirements/.venv/
-#/opt/stack/devstack/stack.sh
+time /opt/stack/devstack/stack.sh
